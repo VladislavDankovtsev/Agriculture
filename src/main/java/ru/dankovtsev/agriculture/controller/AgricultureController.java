424ecbe0bd1e6975909ad9faa381264e8b9931a8
Vldaislav_19 <vladislav.dankovtsev@yandex.ru>
2020-04-02 14:19:29 +0300

    проверка соединения с сервером

--- src/main/java/ru/dankovtsev/agriculture/controller/AgricultureController.java
+++ src/main/java/ru/dankovtsev/agriculture/controller/AgricultureController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.dankovtsev.agriculture.model.Agriculture;
+import ru.dankovtsev.agriculture.model.CheckConnection;
 import ru.dankovtsev.agriculture.service.AgricultureServiceConfig;
 
 @RestController
@@ -22,6 +23,13 @@ public class AgricultureController {
     public Agriculture getAgricultureNow(){
 
         return agricultureServiceConfig.getAgriculture();
+    }
+
+    @RequestMapping(path = "/check", method = RequestMethod.GET)
+    public CheckConnection getAgricultureConnection(){
+        CheckConnection checkConnection = new CheckConnection();
+        checkConnection.setCheck("work");
+        return checkConnection;
     }
 
 }
